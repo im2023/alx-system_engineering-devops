c4d5b4369cb79da1af1abe2da6b5dbc4fd6549b3
imam koutabi <alximamkoutabi@gmail.com>
2024-03-26 02:00:32 +0000

    API

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -1,32 +1,32 @@
 #!/usr/bin/python3
 """returning information about
 TODO list progress"""
-if __name__ == '__main__':
+
     import requests
     import sys
 
-    employee_id = sys.argv[1]
+employeId = sys.argv[1]
 
-    response = requests.get(
-        f'https://jsonplaceholder.typicode.com/users/{employee_id}'
-    )
-    EMPLOYEE_NAME = response.json()['name']
-    tasks_response = requests.get(
-        f'https://jsonplaceholder.typicode.com/todos/?userId={employee_id}'
-    )
-    TOTAL_NUMBER_OF_TASKS = len(tasks_response.json())
-    NUMBER_OF_DONE_TASKS = 0
-    TASK_TITLE = ""
-    for dictionary in tasks_response.json():
-        for key, value in dictionary.items():
-            if key == 'completed' and value is True:
-                NUMBER_OF_DONE_TASKS += 1
-                TASK_TITLE += '\n'
-                TASK_TITLE += '\t '
-                TASK_TITLE += f"{dictionary['title']}"
+rsp = requests.get(
+    f'https://jsonplaceholder.typicode.com/users/{employeId}'
+)
+EMPLOYEE_NAME = rsp.json()['name']
+tasks_response = requests.get(
+    f'https://jsonplaceholder.typicode.com/todos/?userId={employeId}'
+)
+TOTAL_NUMBER_OF_TASKS = len(tasks_response.json())
+NUMBER_OF_DONE_TASKS = 0
+TASK_TITLE = ""
+for dictionary in tasks_response.json():
+    for key, value in dictionary.items():
+        if key == 'completed' and value is True:
+            NUMBER_OF_DONE_TASKS += 1
+            TASK_TITLE += '\n'
+            TASK_TITLE += '\t '
+            TASK_TITLE += f"{dictionary['title']}"
 
-    print(
-        f'Employee {EMPLOYEE_NAME} is done with tasks({NUMBER_OF_DONE_TASKS}/'
-        f'{TOTAL_NUMBER_OF_TASKS}):', end=""
-    )
-    print(TASK_TITLE)
+print(
+    f'Employee {EMPLOYEE_NAME} is done with tasks({NUMBER_OF_DONE_TASKS}/'
+    f'{TOTAL_NUMBER_OF_TASKS}):', end=""
+)
+print(TASK_TITLE)
